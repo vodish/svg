779c067648e7767217b6379b115c5efcdf749979
pkarasev <p.karasev@psw.ru>
2024-03-12 10:48:58 +0300

    upd

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,24 @@ import SvgBook from './comp/svg-book/svg-book'
 export default  function App() {
   const [count, setCount] = createSignal(0)
 
+  function sum(sum: number, percent: number) {
+    const res = (sum / 100) * percent;
+
+    return `(${percent}) от ${sum} = ${res}`
+  }
+
+
   return (
     <>
 
       <div style={{ display: 'flex', gap: '2em' }}>
-
         <SvgBook />
       </div>
+
+
+      <div>{sum(650, 40)}</div>
+      <div>{sum(450, 60)}</div>
+
 
       <div class="card">
         <button onClick={() => setCount((count) => count + 1)}>
